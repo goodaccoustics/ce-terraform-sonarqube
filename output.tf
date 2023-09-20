f09e27eea5f6cd02288ff80f3a3883c5cb2fb2b2
goodaccoustics <justinlim@JustinnoMacBook-Air.local>
2023-09-20 23:27:23 +0800

    output ec2 instance id

--- output.tf
+++ output.tf
@@ -2,6 +2,10 @@ output "ec2-public-ip" {
   value = aws_instance.ec2.public_ip
 }
 
+output "ec2-instance-id" {
+  value = aws_instance.ec2.id
+}
+
 output "ec2-ssh" {
   value = "ssh -i ${local.keypair}.pem ubuntu@${aws_instance.ec2.public_ip}"
 }
